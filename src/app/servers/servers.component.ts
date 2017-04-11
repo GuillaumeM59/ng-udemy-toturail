0c3f9cac8a297bec72605cfefc89160433434500
Guillaumem59 <manier-guillaume@orange.fr>
2017-04-11 15:41:25 +0200

    user ngif ng for ngClass and ng Style

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  AllowNewServer = false;
+  serverCreationStatus = 'No server created!';
+  serverName = 'Servertest';
+  serverCreated = false;
+  servers = [ 'Testeserver', 'TestServerNew']
+  constructor() {
+
+    setTimeout( () => {
+      this.AllowNewServer = true;
+    }, 2000);
+
+  }
 
   ngOnInit() {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server: ' + this.serverName + ' created!';
+    this.servers.push(this.serverName);
+    this.serverCreated = true;
   }
 
 }
